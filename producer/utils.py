67eae92a5781c5940721312514cf774a888afd57
Avinandan Banerjee <avinandanbanerjee99@gmail.com>
2020-09-07 21:09:44 +0530

    Create Producer log directory if it does not exist

--- producer/utils.py
+++ producer/utils.py
@@ -5,7 +5,10 @@ from pathlib import Path
 import logging
 
 PROJECT_HOME_PATH = Path(__file__).parents[1]
-LOGGING_FILE_PATH = os.path.join(PROJECT_HOME_PATH, "logs", "{}.log")
+LOGGING_HOME_PATH = os.path.join(PROJECT_HOME_PATH, "logs")
+if not os.path.exists(LOGGING_HOME_PATH):
+    os.mkdir(LOGGING_HOME_PATH)
+LOGGING_FILE_PATH = os.path.join(LOGGING_HOME_PATH, "{}.log")
 CONFIG_FILE_PATH = os.path.join(PROJECT_HOME_PATH, "config.ini")
 
 
